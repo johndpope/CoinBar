b3d01a5071cb8395feaa5455521cad5b434fd0e7
John Pope <jp@fieldstormapp.com>
2017-10-03 11:42:18 -0400

    add chart to cell.

--- CoinBar/App/Menu/Views/CoinMenuItemView.swift
+++ CoinBar/App/Menu/Views/CoinMenuItemView.swift
@@ -1,11 +1,15 @@
 import Cocoa
+import Charts
 
 final class CoinMenuItemView: MenuItemView, NibLoadable {
     
+    @IBOutlet var lineChartView: LineChartView!
     @IBOutlet private(set) var imageView: NSImageView!
     @IBOutlet private(set) var symbolLabel: NSTextField!
     @IBOutlet private(set) var priceLabel: NSTextField!
     @IBOutlet private(set) var percentChangeLabel: NSTextField!
+    
+    var data = LineChartData()
     
     func configure(with coin: Coin, currency: Preferences.Currency, imagesService: ImagesServiceProtocol) {
                 
@@ -21,6 +25,30 @@ final class CoinMenuItemView: MenuItemView, NibLoadable {
                 self.imageView.image = image
             }
         }
+        
+        
+        // Chart
+        
+        // Do any additional setup after loading the view.
+        let ys1 = Array(1..<10).map { x in return sin(Double(x) / 2.0 / 3.141 * 1.5) }
+        let ys2 = Array(1..<10).map { x in return cos(Double(x) / 2.0 / 3.141) }
+        
+        let yse1 = ys1.enumerated().map { x, y in return ChartDataEntry(x: Double(x), y: y) }
+        let yse2 = ys2.enumerated().map { x, y in return ChartDataEntry(x: Double(x), y: y) }
+        
+        
+        let ds1 = LineChartDataSet(values: yse1, label: "Hello")
+        ds1.colors = [NSUIColor.red]
+        data.addDataSet(ds1)
+        
+        let ds2 = LineChartDataSet(values: yse2, label: "World")
+        ds2.colors = [NSUIColor.blue]
+        data.addDataSet(ds2)
+        self.lineChartView.data = data
+        
+        self.lineChartView.gridBackgroundColor = NSUIColor.white
+        
+        self.lineChartView.chartDescription?.text = "Linechart Demo"
         
         // Value label
         
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+platform :osx, '10.11'
+
+target 'CoinBar' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Charts'
+  # Pods for CoinBar
+
+  target 'CoinBarTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
